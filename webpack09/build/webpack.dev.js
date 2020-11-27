af0002bca8f4f69bea5f83d0c349a0884b8ad6f3
bryce <1907004005@qq.com>
2020-11-27 17:23:44 +0800

    [feature] add multi env wenpack config

---
+++ webpack09/build/webpack.dev.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const commonConifg = require('./webpack.common.js');
+
+const devConfig = {
+  mode: 'development', // development模式
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:3000/',
+    },
+    hot: true,
+    hotOnly: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  optimization: {
+    usedExports: true,
+  },
+};
+
+module.exports = merge(commonConifg, devConfig);
